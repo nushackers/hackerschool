e6015ac8979ff0ef61ed4ece662dfc516290e18a
Michael Yong <wrong92@gmail.com>
2012-09-14 22:10:54 +0800

    css tweaks

--- models/events.py
+++ models/events.py
@@ -2,7 +2,7 @@ eventlist = [
 	{
 		"title" : "Introduction to HTML5/CSS3",
 		"datetime" : "Saturday, 22 September 2012, 10am",
-		"location" : "COM1",
+		"location" : "COM1 (Exact location TBA)",
 		"audience" : "Beginner",
 		"description": "Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero, a pharetra augue. Curabitur blandit tempus porttitor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.",
 		"image": "/static/img/html5.png"
@@ -10,7 +10,7 @@ eventlist = [
 	{
 		"title" : "Git & jQuery for beginners",
 		"datetime" : "Monday, 24 September 2012, 10am",
-		"location" : "COM1",
+		"location" : "COM1 (Exact location TBA)",
 		"audience" : "Beginner",
 		"description": "Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero, a pharetra augue. Curabitur blandit tempus porttitor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.",
 		"image": "/static/img/gitjs.png"
