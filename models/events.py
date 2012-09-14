b8e62bb2ba26ce2c3cf81dcfbde9c3044061b4e5
Michael Yong <wrong92@gmail.com>
2012-09-15 02:13:46 +0800

    updated gforms link

--- models/events.py
+++ models/events.py
@@ -12,7 +12,7 @@ eventlist = [
 		"datetime" : "Monday, 24 September 2012, 10am",
 		"location" : "COM1 (Exact location TBA)",
 		"audience" : "Beginner",
-		"description": "Git will change your life. Seriously. <a class='calltoaction' href='https://docs.google.com/spreadsheet/viewform?formkey=dFJLT2NGUWxJM2tXODlVMDlmUS1JckE6MA'>Sign Up Here!</a>",
+		"description": "Git will change your life. Seriously. <a class='calltoaction' href='https://docs.google.com/spreadsheet/viewform?formkey=dG1DVmx6V3VWUE9jYTlKUzVSUGhRcWc6MQ'>Sign Up Here!</a>",
 		"image": "/static/img/gitjs.png"
 	}
 ]
