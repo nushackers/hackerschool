f37fb754bb84362cab3b103bf03c44a4801fa5f6
Michael Yong <wrong92@gmail.com>
2012-09-14 12:53:49 +0800

    basic styles

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+# import events list
+from models import events
+from models import about
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template(
+    	'index.html',
+    	preamble = about.preamble,
+    	events = events.eventlist
+    )
 
 if __name__ == "__main__":
     app.run()
---
+++ models/events.py
@@ -0,0 +1,18 @@
+eventlist = [
+	{
+		"title" : "Introduction to HTML5/CSS3",
+		"datetime" : "Saturday, 22 September 2012, 10am",
+		"location" : "COM1",
+		"audience" : "Beginner",
+		"description": "",
+		"image": "/static/img/html5.png"
+	},
+	{
+		"title" : "Git & jQuery for beginners",
+		"datetime" : "Monday, 24 September 2012, 10am",
+		"location" : "COM1",
+		"audience" : "Beginner",
+		"description": "",
+		"image": "/static/img/gitjs.png"
+	}
+]
