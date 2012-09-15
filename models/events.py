536fa1fb2643ecad0f134417ecfbfbc5896c28e6
Michael Yong <wrong92@gmail.com>
2012-09-15 14:00:48 +0800

    added google analytics to the page

--- models/events.py
+++ models/events.py
@@ -1,16 +1,16 @@
 eventlist = [
 	{
 		"title" : "Introduction to HTML5/CSS3",
-		"datetime" : "Saturday, 22 September 2012, 10am",
-		"location" : "COM1 (Exact location TBA)",
+		"datetime" : "Saturday, 22 September 2012, 10am - 1pm",
+		"location" : "NUS COM1 (Exact location TBA)",
 		"audience" : "Beginner",
 		"description": "You wanted to make a website, but found w3c's tutorials too boring? Come down to get a hands on experience on designing beautiful webpages. <a class='calltoaction' href='https://docs.google.com/spreadsheet/viewform?formkey=dG1DVmx6V3VWUE9jYTlKUzVSUGhRcWc6MQ'>Sign Up Here!</a>",
 		"image": "/static/img/html5.png"
 	},
 	{
 		"title" : "Git & jQuery for beginners",
-		"datetime" : "Monday, 24 September 2012, 10am",
-		"location" : "COM1 (Exact location TBA)",
+		"datetime" : "Monday, 24 September 2012, 10am - 1pm",
+		"location" : "NUS COM1 (Exact location TBA)",
 		"audience" : "Beginner",
 		"description": "Git's a version control system. Sounds boring we know, but it will change your life. Seriously. Also meet jquery, a swiss army knife for frontend web dev . <a class='calltoaction' href='https://docs.google.com/spreadsheet/viewform?formkey=dG1DVmx6V3VWUE9jYTlKUzVSUGhRcWc6MQ'>Sign Up Here!</a>",
 		"image": "/static/img/gitjs.png"
