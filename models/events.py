17dfb8dd689d2c307baadb670f44e55d0d499521
Michael Yong <wrong92@gmail.com>
2012-09-16 19:10:21 +0800

    updated descriptions and added payment info

--- models/events.py
+++ models/events.py
@@ -1,18 +1,18 @@
 eventlist = [
-	{
-		"title" : "Introduction to HTML5/CSS3",
-		"datetime" : "Saturday, 22 September 2012, 10am - 1pm",
-		"location" : "NUS COM1 (Exact location TBA)",
-		"audience" : "Beginner",
-		"description": "Wanted to make a website, but found w3c's tutorials too boring? We'll teach you HTML5 and CSS3 from scratch. Come down to get a hands-on experience on designing beautiful webpages, and <strong>walk away with a basic, personalized website.</strong> <a class='calltoaction' href='https://docs.google.com/spreadsheet/viewform?formkey=dG1DVmx6V3VWUE9jYTlKUzVSUGhRcWc6MQ'>Sign Up Here!</a>",
-		"image": "/static/img/html5.png"
-	},
-	{
-		"title" : "Git & jQuery for beginners",
-		"datetime" : "Monday, 24 September 2012, 10am - 1pm",
-		"location" : "NUS COM1 (Exact location TBA)",
-		"audience" : "Beginner",
-		"description": "Git is a version control system. Sounds boring, we know, but it'll change the way you code. Seriously. Also meet jQuery, a swiss army knife for frontend web development. <strong>Walk away with a snazzy portfolio site, backed up on Github.</strong> <a class='calltoaction' href='https://docs.google.com/spreadsheet/viewform?formkey=dG1DVmx6V3VWUE9jYTlKUzVSUGhRcWc6MQ'>Sign Up Here!</a>",
-		"image": "/static/img/gitjs.png"
-	}
+    {
+        "title": "Introduction to HTML5/CSS3",
+        "datetime": "Saturday, 22 September 2012, 10am - 1pm",
+        "location": "NUS COM1 (Exact location TBA)",
+        "audience": "Beginner",
+        "description": "Wanted to make a website, but found w3c's tutorials too boring? We'll teach you HTML5 and CSS3 from scratch. Come down to get a hands-on experience on designing beautiful webpages, and <strong>walk away with a basic, personalized website.</strong> <a class='calltoaction' href='https://docs.google.com/spreadsheet/viewform?formkey=dG1DVmx6V3VWUE9jYTlKUzVSUGhRcWc6MQ'>Sign Up Here!</a>",
+        "image": "/static/img/html5.png"
+    },
+    {
+        "title": "Git & jQuery for beginners",
+        "datetime": "Monday, 24 September 2012, 10am - 1pm",
+        "location": "NUS COM1 (Exact location TBA)",
+        "audience": "Beginner",
+        "description": "Git is a version control system. It'll change the way you code. Seriously. Also meet jQuery, a swiss army knife for frontend web development. <strong>You'll wonder what you were doing before git.</strong> <a class='calltoaction' href='https://docs.google.com/spreadsheet/viewform?formkey=dG1DVmx6V3VWUE9jYTlKUzVSUGhRcWc6MQ'>Sign Up Here!</a>",
+        "image": "/static/img/gitjs.png"
+    }
 ]
