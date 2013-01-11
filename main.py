d354ab038925a8dd799f1cbd36eb89696cd77ada
Michael Yong <wrong92@gmail.com>
2013-01-12 01:51:44 +0800

    added coming semester's information

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ app = Flask(__name__)
 # import events list
 from models import events
 
+
 @app.route('/')
 def index():
     return render_template(
         'index.html',
-        pastevents = events.pastevents,
-        upcomingevents = events.upcomingevents
+        pastevents=events.pastevents,
+        upcomingevents=events.upcomingevents
     )
+
 
 @app.route('/about')
 def aboutus():
@@ -24,9 +26,10 @@ def aboutus():
 def schedule():
     return render_template(
         'schedule.html',
-        pastevents = events.pastevents,
-        upcomingevents = events.upcomingevents
+        pastevents=events.pastevents,
+        upcomingevents=events.upcomingevents
     )
+
 
 @app.route('/materials')
 def materials():
@@ -34,17 +37,20 @@ def materials():
         'materials.html'
     )
 
+
 @app.route('/materials/html5')
 def html_workshop():
     return render_template(
         'materials/html5workshop.html'
     )
 
+
 @app.route('/materials/gitjquery')
 def gitjquery():
     return render_template(
         'materials/gitjquery.html'
     )
+
 
 if __name__ == "__main__":
     app.run()
--- models/events.py
+++ models/events.py
@@ -1,4 +1,38 @@
-upcomingevents = []
+upcomingevents = [
+    {
+        "title": "Introduction to HTML5/CSS3",
+        "datetime": "Saturday, 26 January 2013, 10am - 1pm",
+        "location": "TBC",
+        "audience": "Beginner",
+        "description": "Wanted to make a website, but found w3c's tutorials too boring? We'll teach you HTML5 and CSS3 from scratch. Come down to get a hands-on experience on designing beautiful webpages, and <strong>walk away with a personalized website.</strong>",
+        "image": "/static/img/html5.png"
+    },
+    {
+        "title": "Git for beginners",
+        "datetime": "Saturday, 2 February 2013, 10am - 1pm",
+        "location": "TBC",
+        "audience": "Beginner",
+        "description": "Details to come.",
+        "image": "/static/img/git.png"
+    },
+    {
+        "title": "Web Development first steps with Sinatra",
+        "datetime": "Saturday, 16 February 2013, 10am - 1pm",
+        "location": "TBC",
+        "audience": "Beginner",
+        "prereq": "HTML/CSS, and programming basics is assumed. Familiarity with Ruby is not required.",
+        "description": "Details to come.",
+        "image": "/static/img/sinatra.png"
+    },
+    {
+        "title": "Unix and Shell Scripting Basics",
+        "datetime": "Saturday, 23 February 2013, 10am - 1pm",
+        "location": "TBC",
+        "audience": "Beginner",
+        "description": "Details to come.",
+        "image": "/static/img/unix.png"
+    }
+]
 
 pastevents = [
     {
@@ -7,7 +41,8 @@ pastevents = [
         "location": "NUS COM1-02-12 SR3 ",
         "audience": "Beginner",
         "description": "jQuery is a swiss army knife for frontend web development. Also, meet Git, a version control system. It'll change the way you code. Seriously. <strong> You'll wonder what you were doing before Git. </strong><a class='calltoaction' href='/materials/gitjquery'>materials here</a>",
-        "image": "/static/img/gitjs.png"
+        "image": "/static/img/gitjs.png",
+        "materials": "</strong><a class='calltoaction' href='/materials/gitjquery'>Materials here</a>"
     },
     {
         "title": "Introduction to HTML5/CSS3",
@@ -15,7 +50,7 @@ pastevents = [
         "location": "NUS COM1-02-12 SR3 ",
         "audience": "Beginner",
         "description": "Wanted to make a website, but found w3c's tutorials too boring? We'll teach you HTML5 and CSS3 from scratch. Come down to get a hands-on experience on designing beautiful webpages, and <strong>walk away with a personalized website.</strong> <a class='calltoaction' href='/materials/html5'>materials here</a>",
-        "image": "/static/img/html5.png"
+        "image": "/static/img/html5.png",
+        "materials": "</strong><a class='calltoaction' href='/materials/html5'>Materials here</a>"
     },
-
 ]
