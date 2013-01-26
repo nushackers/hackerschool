12f06a87b1c45cfce21b6d63d86c2c18c69bb325
Eli James <ejames@pandamian.com>
2013-01-26 11:22:01 +0800

    Updated git description, removed signup link for html5, updated personal.zip location

--- models/events.py
+++ models/events.py
@@ -4,7 +4,7 @@ upcomingevents = [
         "datetime": "Saturday, 26 January 2013, 10am - 1pm",
         "location": "Seminar Room 3, COM1, School of Computing, NUS",
         "audience": "Beginner",
-        "description": "Wanted to make a website, but found w3c's tutorials too boring? We'll teach you HTML5 and CSS3 from scratch. Come down to get a hands-on experience on designing beautiful webpages, and <strong>walk away with a personalized website.</strong> <a href='https://docs.google.com/a/linuxnus.org/spreadsheet/viewform?formkey=dDVvX0JKQVZBQnZob3ZRdW1pVERLNXc6MQ'>Sign up here &rarr;</a>",
+        "description": "Wanted to make a website, but found w3c's tutorials too boring? We'll teach you HTML5 and CSS3 from scratch. Come down to get a hands-on experience on designing beautiful webpages, and <strong>walk away with a personalized website.</strong> Workshop complete; <a href='/materials/html5'>materials here</a>.",
         "image": "/static/img/html5.png"
     },
     {
@@ -12,7 +12,7 @@ upcomingevents = [
         "datetime": "Saturday, 2 February 2013, 10am - 1pm",
         "location": "Global Learning Room, Education Resource Centre, University Town",
         "audience": "Beginner",
-        "description": "Ever used Dropbox to store your code because it makes backups? Made multiple copies of your assignment’s folder just to try different things? Worked on group projects by coding in Google Docs? Git is the tool for you: <strong>it’ll change the way you code for the better.</strong> <a href='https://docs.google.com/a/linuxnus.org/spreadsheet/viewform?formkey=dDVvX0JKQVZBQnZob3ZRdW1pVERLNXc6MQ'>Sign up here &rarr;</a>",
+        "description": "Ever used Dropbox to store your code because it makes backups? Made multiple copies of your assignment's folder just to try different things? Worked on group projects by coding in Google Docs? Git is the tool for you: <strong>it'll change the way you code for the better.</strong> <a href='https://docs.google.com/a/linuxnus.org/spreadsheet/viewform?formkey=dDVvX0JKQVZBQnZob3ZRdW1pVERLNXc6MQ'>Sign up here &rarr;</a>",
         "image": "/static/img/git.png"
     },
     {
