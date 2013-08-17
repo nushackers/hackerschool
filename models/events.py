3479ad77ffd36b5243b275cd24a4edd636ddd42e
Jerome Cheng <jcheng31@gmail.com>
2013-08-17 18:52:36 +0800

    Remove "(event concluded)" - these are already in "Past Events".

--- models/events.py
+++ models/events.py
@@ -39,7 +39,7 @@ pastevents = [
     },
     {
         "title": "Git & jQuery for beginners",
-        "datetime": "Monday, 24 September 2012, 10am - 1pm (Event concluded)",
+        "datetime": "Monday, 24 September 2012, 10am - 1pm",
         "location": "NUS COM1-02-12 SR3 ",
         "audience": "Beginner",
         "description": "jQuery is a swiss army knife for frontend web development. Also, meet Git, a version control system. It'll change the way you code. Seriously. <strong> You'll wonder what you were doing before Git. </strong><a class='calltoaction' href='/materials/gitjquery'>materials here</a>",
@@ -48,7 +48,7 @@ pastevents = [
     },
     {
         "title": "Introduction to HTML5/CSS3",
-        "datetime": "Saturday, 22 September 2012, 10am - 1pm  (Event concluded)",
+        "datetime": "Saturday, 22 September 2012, 10am - 1pm",
         "location": "NUS COM1-02-12 SR3 ",
         "audience": "Beginner",
         "description": "Wanted to make a website, but found w3c's tutorials too boring? We'll teach you HTML5 and CSS3 from scratch. Come down to get a hands-on experience on designing beautiful webpages, and <strong>walk away with a personalized website.</strong> <a class='calltoaction' href='/materials/html5_2012'>materials here</a>",
