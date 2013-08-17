8b8c2a5382598654fa78fb715283a39a56b51f65
Jerome Cheng <jcheng31@gmail.com>
2013-08-17 19:21:25 +0800

    Move Shell and Sinatra to pastevents.

--- models/events.py
+++ models/events.py
@@ -1,4 +1,8 @@
 upcomingevents = [
+
+]
+
+pastevents = [
     {
         "title": "Web Development First Steps with Sinatra",
         "datetime": "Saturday, 16 February 2013, 10am - 1pm",
@@ -6,7 +10,8 @@ upcomingevents = [
         "audience": "Beginner",
         "prereq": "HTML/CSS, and programming basics is assumed. Familiarity with Ruby is not required.",
         "description": "Sinatra is a small but powerful micro-framework for creating web applications in Ruby. In this workshop, we'll take a whirlwind tour of Sinatra and in the process, learn about the common MVC web application pattern. This workshop will also cover the basic ideas necessary for building web applications (whether in Sinatra or otherwise).<br/><a href='https://docs.google.com/a/linuxnus.org/spreadsheet/viewform?formkey=dDVvX0JKQVZBQnZob3ZRdW1pVERLNXc6MQ'>Sign up here &rarr;</a>",
-        "image": "/static/img/sinatra.png"
+        "image": "/static/img/sinatra.png",
+        "materials": "Materials to come."
     },
     {
         "title": "Unix and Shell Scripting Basics",
@@ -14,11 +19,9 @@ upcomingevents = [
         "location": "Global Learning Room, Education Resource Centre, University Town",
         "audience": "Beginner",
         "description": "In the professional world, unix is ubiquitous, and shell scripting is the staple of unix. With shell scripting, you can automate tasks that'd otherwise take you hours to accomplish. We'll be explaining the basic philosophy & ideas behind the design of Unix, and introducing shell scripting by way of walking through some very common tasks.<br/><a href='https://docs.google.com/a/linuxnus.org/spreadsheet/viewform?formkey=dDVvX0JKQVZBQnZob3ZRdW1pVERLNXc6MQ'>Sign up here &rarr;</a>",
-        "image": "/static/img/freeBSD.png"
-    }
-]
-
-pastevents = [
+        "image": "/static/img/freeBSD.png",
+        "materials": "Materials to come."
+    },
     {
         "title": "Git for Beginners",
         "datetime": "Saturday, 2 February 2013, 10am - 1pm",
