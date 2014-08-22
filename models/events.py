a10f1e55bdf897613a10c754a05d9e52743c9aa7
Yujian <yyjhao@gmail.com>
2014-08-22 12:21:57 +0800

    update

--- models/events.py
+++ models/events.py
@@ -1,4 +1,37 @@
 upcomingevents = [
+  {
+    "title": "Introduction to HTML/CSS",
+    "datetime": "Saturday, 30 August 2014, 2:30pm - 5:30pm",
+    "location": "Seminar Room 3 (COM1-02-12), School of Computing, National University of Singapore",
+    "audience": "Beginner, no prior knowledge assumed",
+    "description": "Want to make a website, but found w3c's tutorials too boring? We'll teach you HTML and CSS from scratch. Come down to get hands-on experience in designing beautiful webpages, and walk away with a basic, personalized website. <br /><a href='https://docs.google.com/forms/d/1jM4HjcXShGPFG2l4RtoAzoCduGKwCAGVWr77BMnEVQA/'>Sign up here &rarr;</a>",
+    "image": "/static/img/html5.png"
+  },
+  {
+    "title": "Git for Beginners",
+    "datetime": "Saturday, 6 September 2014, 10:00am - 1:00pm",
+    "location": "TBD",
+    "audience": "Beginner",
+    "description": "Ever used Dropbox to store your code because it makes backups? Made multiple copies of your assignment's folder just to try different things? Worked on group projects by coding in Google Docs? Git is the tool for you: <strong>it'll change the way you code for the better.</strong>",
+    "image": "/static/img/git.png"
+  },
+  {
+    "title": "Introduction to JavaScript and Interactive Web Apps",
+    "datetime": "Saturday, 13 September 2014, 10:00am - 1:00pm",
+    "location": "TBD",
+    "audience": "Beginner",
+    "description": "Web browsers are everywhere, and so is JavaScript, a dynamic programming language that browsers implement. We will be covering the basics of JavaScript and how to use it to control and add complex behaviour to web pages. In the process, we'll go through the development of an interactive web application.",
+    "image": "/static/img/javascript.png"
+  },
+  {
+    "title": "Web Development with Express",
+    "datetime": "Saturday, 20 September 2014, 10:00am - 1:00pm",
+    "location": "TBD",
+    "audience": "Beginner",
+    "prereq": "Familiarity with HTML/CSS and programming is expected. Knowledge of JavaScript is a plus, but is not required.",
+    "description": "Express is a small but powerful micro-framework for creating web applications in Node.js. In this workshop, we'll take a whirlwind tour of Express and, in the process, learn about the common MVC web application pattern. We'll also cover the basic ideas necessary for building web applications (whether in Express or otherwise).",
+    "image": "/static/img/express.png"
+  },
 ]
 
 pastevents = [
