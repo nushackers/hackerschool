e57e306530dda1a777c933c8331b830c6de1c944
Ng Zhi An <ngzhian@gmail.com>
2015-09-07 14:36:11 +0000

    Remove hard coded routes in main.py

--- main.py
+++ main.py
@@ -43,48 +43,9 @@ def materials():
     return render_template('materials.html')
 
 
-@app.route('/materials/railsworkshop2015/')
-def materials_rails_workshop2015():
-    return render_template('materials/railsworkshop2015.html')
-
-@app.route('/materials/expressworkshop2014/')
-def materials_express_workshop2014():
-    return render_template('materials/expressworkshop2014.html')
-
-
-@app.route('/materials/jsworkshop2014/')
-def materials_js_workshop2014():
-    return render_template('materials/jsworkshop2014.html')
-
-
-@app.route('/materials/gitworkshop2014/')
-def materials_git_workshop2014():
-    return render_template('materials/gitworkshop2014.html')
-
-
-@app.route('/materials/html5workshop2014/')
-def materials_html_workshop2014():
-    return render_template('materials/html5workshop2014.html')
-
-
-@app.route('/materials/sinatra/')
-def materials_sinatra():
-    return render_template('materials/sinatra.html')
-
-
-@app.route('/materials/html5_2013/')
-def html_2013_workshop():
-    return render_template('materials/html5workshop2013.html')
-
-
-@app.route('/materials/html5_2012/')
-def materials_html_workshop():
-    return render_template('materials/html5workshop.html')
-
-
-@app.route('/materials/gitjquery/')
-def materials_gitjquery():
-    return render_template('materials/gitjquery.html')
+@app.route('/materials/<material>/')
+def materials_gitjquery(material):
+    return render_template('materials/%s.html' % material)
 
 
 if __name__ == "__main__":
