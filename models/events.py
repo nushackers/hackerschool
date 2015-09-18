b199184a3ff0e374c3918243e50ce7c1fa35b4ba
Ng Zhi An <ngzhian@gmail.com>
2015-09-18 02:30:26 +0000

    Fix typo in venue for hackerschool

--- models/events.py
+++ models/events.py
@@ -3,7 +3,7 @@ upcomingevents = [
 {
     "title": "Introduction to HTML/CSS",
     "datetime": "Saturday, 19 September 2015, 1:00pm - 4:00pm",
-    "location": "SR, Town Plaza (Level 2), University Town, National University of Singapore",
+    "location": "SR3, Town Plaza (Level 2), University Town, National University of Singapore",
     "audience": "Beginners from any faculty or the public, no prior knowledge assumed",
     "description": "Want to make a website, but found w3c's tutorials too boring? We'll teach you HTML and CSS from scratch. Come down to get hands-on experience in designing beautiful webpages, and walk away with a basic, personalized website. <br /><a href='http://goo.gl/forms/hMZo03DZWH'>Sign up here &rarr;</a>",
     "image": "/static/img/html5.png",
@@ -12,7 +12,7 @@ upcomingevents = [
 {
     "title": "Git for Beginners",
     "datetime": "Saturday, 26 September 2015, 1:00pm - 4:00pm",
-    "location": "SR, Town Plaza (Level 2), University Town, National University of Singapore",
+    "location": "SR3, Town Plaza (Level 2), University Town, National University of Singapore",
     "audience": "Beginners from any faculty or the public",
     "description": "Ever used Dropbox to store your code because it makes backups? Made multiple copies of your assignment's folder just to try different things? Worked on group projects by coding in Google Docs? Git is the tool for you: it'll change the way you code for the better.<br>Sign ups opening soon.",
     "image": "/static/img/git.png",
@@ -21,7 +21,7 @@ upcomingevents = [
 {
     "title": "JavaScript and Interactive Web Apps",
     "datetime": "Saturday, 3 October 2015, 1:00pm - 4:00pm",
-    "location": "SR, Town Plaza (Level 2), University Town, National University of Singapore",
+    "location": "SR3, Town Plaza (Level 2), University Town, National University of Singapore",
     "audience": "Beginners from any faculty or the public",
     "description": "Web browsers are everywhere, and so is JavaScript, a dynamic programming language that browsers implement. We will be covering the basics of JavaScript and how to use it to control and add complex behaviour to web pages. In the process, we'll go through the development of an interactive web application.<br>Sign ups opening soon.",
     "image": "/static/img/javascript.png",
@@ -30,7 +30,7 @@ upcomingevents = [
 {
     "title": "Web Development with Express",
     "datetime": "Saturday, 17 October 2015, 1:00pm - 4:00pm",
-    "location": "SR, Town Plaza (Level 2), University Town, National University of Singapore",
+    "location": "SR3, Town Plaza (Level 2), University Town, National University of Singapore",
     "audience": "Beginners from any faculty or the public",
     "prereq": "Familiarity with HTML/CSS and programming is expected. Knowledge of JavaScript is a plus, but is not required.",
     "description": "Express is a small but powerful micro-framework for creating web applications in Node.js. In this workshop, we'll take a whirlwind tour of Express and, in the process, learn about the common MVC web application pattern. We'll also cover the basic ideas necessary for building web applications (whether in Express or otherwise). <br />Sign ups opening soon.",
